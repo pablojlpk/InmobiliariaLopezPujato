315c9446d85d7118fb8f7621e53bcf4964762522
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2024-03-21 20:04:40 -0300

    -

---
+++ Controllers/InquilinoController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using InmobiliariaLopezPujato.Models;
+
+namespace InmobiliariaLopezPujato.Controllers;
+
+public class InquilinoController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public InquilinoController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Propietario()
+    {
+        return View();
+    }
+
+public IActionResult Inquilino()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
